adc88bfa1d12c71a55aff4172bbabf7c517550c8
EzzaharAimad <imadezzahar@gmail.com>
2022-11-02 20:52:27 +0100

    Used RestTemplate to call Rating Data Service

--- movie-catalog-service/src/main/java/com/moviecatalog/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/moviecatalog/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,10 +2,19 @@ package com.moviecatalog.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
 
+	@Bean //A PRODUCER, it tells spring execute this method and save this instance somwhere waiting for autowired (The consumer) to ask for it
+	//The time of bean's execution may be different from eager to lazy, but it executes only once cuz it's singleton by default
+	//lazy initialization (when it's configured) : when an autowired is detected spring fetch an instance of the autowired method, then execute the bean that returns the same type once the bean is found
+	//by default it's eager initialization : all the defined beans, and their dependencies are created when the application context is created.
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
